d6b00423cfa9c1c13a6e502fa9d0378dca9919a4
Aleksandr Popov <alex@apopov.eu>
2024-02-11 05:45:41 +0100

    Extra utilities

--- defs.cpp
+++ defs.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2022 Aleksandr Popov
+/* Copyright 2022, 2023, 2024 Aleksandr Popov
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option)
@@ -12,6 +12,9 @@
 
 #include "defs.hpp"
 
+#include <cassert>
+#include <type_traits>
+
 namespace dp {
     std::size_t hash_helper(Loc a, Loc b) noexcept {
         auto i = static_cast<std::make_unsigned_t<Loc>>(a);
@@ -20,12 +23,51 @@ namespace dp {
         return static_cast<std::size_t>(i) << sizeof(i) * 8 | j;
     }
 
-    std::size_t LocHash::operator()(std::pair<Loc, Loc> const&
-            p) const noexcept {
+    std::size_t LocHash::operator()(Cell const& p) const noexcept {
         return hash_helper(p.first, p.second);
     }
 
-    Loc operator"" _loc(unsigned long long l) {
+    Loc operator ""_loc(unsigned long long l) {
         return static_cast<Loc>(l);
+    }
+}
+
+namespace util {
+    Probs normalise(Visits const& v, Cell const& s) {
+        Probs res;
+        Cnt total = v.at(s);
+        for (auto const& [cell, cnt]: v)
+            res[cell] = divide(cnt, total);
+        return res;
+    }
+
+    Frac divide(Cnt const& num, Cnt const& den) {
+        Frac res(num, den);
+        res.canonicalize();
+        return res;
+    }
+
+    double getd(Frac const& fr) {
+        return fr.get_d();
+    }
+}
+
+namespace map {
+    std::size_t TrajHash::operator()(BridgeID const& t) const noexcept {
+        ::dp::LocHash lhasher;
+        auto [first, last, time] = t;
+        // https://stackoverflow.com/a/1646913/126995
+        std::size_t ret{17};
+        ret = 31 * ret + lhasher(first);
+        ret = 31 * ret + lhasher(last);
+        ret = 31 * ret + time;
+        return ret;
+    }
+
+    BridgeID to_bridge_id(Meas const& a, Meas const& b) {
+        auto const& [t1, x1, y1] = a;
+        auto const& [t2, x2, y2] = b;
+        assert(t2 > t1);
+        return {{x1, y1}, {x2, y2}, t2 - t1};
     }
 }
--- defs.hpp
+++ defs.hpp
@@ -1,4 +1,4 @@
-/* Copyright 2022 Aleksandr Popov
+/* Copyright 2022, 2023, 2024 Aleksandr Popov
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option)
@@ -16,13 +16,26 @@
 #include <cstddef>
 #include <cstdint>
 #include <gmpxx.h>
+#include <tuple>
 #include <type_traits>
+#include <unordered_map>
 #include <utility>
+#include <vector>
 
+/* In case you want to use different numeric types, this header and its
+ * implementation are hopefully the only files that need to be rewritten.
+ * For example, for `Frac` that is `double`, `getd` becomes a pass-through
+ * function, and divide should return `res.get_d();` instead.
+ */
 namespace dp {
-	using Cnt = mpz_class;
+    /// Count in a cell.
+    using Cnt = mpz_class;
+    /// Time steps.
     using Time = std::uint32_t;
+    /// Discrete x/y coordinate.
     using Loc = std::int32_t;
+    /// Discrete location.
+    using Cell = std::pair<Loc, Loc>;
 
     /**
      * @brief Hash for a coordinate pair.
@@ -33,12 +46,80 @@ namespace dp {
     std::size_t hash_helper(Loc a, Loc b) noexcept;
 
     /**
-     * Hasher for unordered_map of pairs.
+     * Hasher for std::unordered_map of coordinate pairs.
      */
     struct LocHash {
-        std::size_t operator()(std::pair<Loc, Loc> const& p) const noexcept;
+        std::size_t operator()(Cell const& p) const noexcept;
     };
 
-    Loc operator"" _loc(unsigned long long l);
+    /// Flat DS storing visit counts to specific cells.
+    using Visits = std::unordered_map<Cell, Cnt, LocHash>;
+
+    /**
+     * Literal of type Loc.
+     */
+    Loc operator ""_loc(unsigned long long l);
+}
+
+namespace util {
+    using ::dp::Cell, ::dp::LocHash, ::dp::Visits, ::dp::Cnt;
+    /// Fraction of `Cnt`s.
+    using Frac = mpq_class;
+    /// Flat DS storing visit probabilities (exactly with GMP).
+    using Probs = std::unordered_map<Cell, Frac, LocHash>;
+
+    /**
+     * @brief Normalise a `Visits` map to the value in the cell `s`.
+     * @param v A valid visit probability map, like the one returned by
+     * `dp::DP::flatten`.
+     * @param s The start cell whose value should be normalised to 1.
+     * @return The normalised map (using fractions currently).
+     */
+    Probs normalise(Visits const& v, Cell const& s);
+
+    /**
+     * @brief For GMP integers, we need a different implementation for
+     * division, going through `mpq_class`.
+     * @param num The numerator.
+     * @param den The denominator.
+     * @return num / den.
+     */
+    Frac divide(Cnt const& num, Cnt const& den);
+
+    /**
+     * @brief For GMP fractions, we need to call a special functions to
+     * approximately convert them to `double`.
+     * @param fr The fraction.
+     * @return The closest representation of `fr`.
+     */
+    double getd(Frac const& fr);
+}
+
+namespace map {
+    using ::dp::Loc, ::dp::Time, ::dp::Cell, ::dp::Cnt, ::dp::Visits,
+        ::util::Frac, ::util::Probs;
+    /// Measurement: discrete location with a timestamp.
+    using Meas = std::tuple<Time, Loc, Loc>;
+    /// Trajectory: sequence of measurements.
+    using Traj = std::vector<Meas>;
+    /// Identifier for bridges: start, end, and duration.
+    using BridgeID = std::tuple<Cell, Cell, Time>;
+    /// Identifier for subtrajectories: trajectory ID, start, and end indices.
+    using SubTraj = std::tuple<std::uint32_t, std::size_t, std::size_t>;
+
+    /**
+     * Hasher for std::undordered_map of bridge identifiers.
+     */
+    struct TrajHash {
+        std::size_t operator()(BridgeID const& t) const noexcept;
+    };
+
+    /**
+     * @brief Get a bridge ID when going from `a` to `b`.
+     * @param a The start of the bridge.
+     * @param b The end of the bridge.
+     * @return The bridge ID for going from `a` to `b` in correct time.
+     */
+    BridgeID to_bridge_id(Meas const& a, Meas const& b);
 }
 #endif
